1295b94db80e89e63288cb7c51dbd97f8b804071
Cristina Botez <cristina0botez@gmail.com>
2025-05-05 17:34:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from os import environ, path
+from json import loads
 
 from dotenv import load_dotenv
 
 # Load variables from .env
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, ".env"))
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, ".env"))
 
 
 class Config:
@@ -12,9 +13,10 @@ class Config:
 
     # General Config
     FLASK_APP = "wsgi.py"
-    FLASK_DEBUG = environ.get("FLASK_DEBUG")
+    FLASK_DEBUG = loads(environ.get("FLASK_DEBUG"))
     SECRET_KEY = environ.get("SECRET_KEY")
 
     # Static Assets
     STATIC_FOLDER = "static"
     TEMPLATES_FOLDER = "templates"
+    PRODUCT_DATA_FILEPATH = f"{BASE_DIR}/data/products.json"
